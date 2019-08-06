b38c592d711634ec953e57b84331b292e7303b84
marcus110379 <marcusjmadden@gmail.com>
2019-08-06 16:03:11 -0500

    problems

---
+++ index.js
@@ -0,0 +1,7 @@
+// JavaScript-Basic1-CW
+alert("Hello World");
+var nameVariable = "Marcus";
+alert("Hello " + nameVariable);
+dogAge = 3;
+alert("my dog age is " + dogAge * 7);
+alert("1 + 1 = " + (1+1));
